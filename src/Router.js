6ea3f8f5f0830849220df8232b1995515b686e45
happybae <qo98333@naver.com>
2022-06-06 14:23:37 +0900

    VCRequest

--- src/Router.js
+++ src/Router.js
@@ -9,6 +9,7 @@ import SearchInfo from "./Routes/SearchInfo";
 import Contract from "./Routes/Contract";
 import MyPage from "./Routes/MyPage";
 
+
 const AppRouter = () => {
     return (
         <Router>
@@ -19,7 +20,7 @@ const AppRouter = () => {
                 <Route path="/DrawResume" element={<DrawResume/>}></Route>
                 <Route path="/DrawPost" element={<DrawPost/>}></Route>
                 <Route path="/:cata/:no" element={<PostInfo/>}></Route>
-                <Route path="/:brandname" element={<SearchInfo/>}></Route>
+                {/* <Route path="/:brandname" element={<SearchInfo/>}></Route> */}
                 <Route path="/MyPage" element={<MyPage/>}></Route>
                 <Route path="/:cata/:no/Contract" element={<Contract/>}></Route>
             </Routes>        
